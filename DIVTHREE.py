732ab8eff5cac60519283ab8a845f577907d8570
Harishankar S <67646220+Hurry-Nair@users.noreply.github.com>
2021-01-03 18:33:40 +0530

    DIVTHREE_Solved

--- DIVTHREE.py
+++ DIVTHREE.py
@@ -3,6 +3,21 @@
 t = int(input())
 
 for i in range(t):
+    #map input to variables
     n, k, d = map(int, input().split())
+
     problems = 0 #total number of problems
     s = input().split()
+    
+    #Find number of problems available
+    for i in range(n):
+        problems += int(s[i])
+
+    #Check number of possible contests with available problems
+    days_possible = problems//k
+
+    #Compare required days with possible days
+    if days_possible >= d:
+        print(d)
+    elif days_possible < d:
+        print(days_possible)
