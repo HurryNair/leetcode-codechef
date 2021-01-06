0e282baafb858d5625bcde3f621b3d43bea06241
Harishankar S <67646220+Hurry-Nair@users.noreply.github.com>
2021-01-06 21:07:25 +0530

    BILLRD_pseudo

---
+++ BILLRD.py
@@ -0,0 +1,25 @@
+#Filename : BILLRD.py
+
+t = int(input())
+
+for i in range(t):
+    n, k, x, y = map(int, input().split())
+
+    # if ball hits the right side of the table
+    # decrement x & increment y
+    # increment collission 
+
+    # if ball hits the top side of the table
+    # decrement x & decrement y
+    # increment collission
+ 
+    # if ball hits the left side of the table
+    # increment x & decrement y
+    # increment collission
+    
+    # if ball hits the bottom side of the table
+    # increment x & increment y 
+    # increment collission  
+
+
+
