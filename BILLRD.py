479870abc180a1c03284b018ea2e553575f181a9
Harishankar S <67646220+Hurry-Nair@users.noreply.github.com>
2021-01-07 15:45:46 +0530

    BILLRD_O(1)

--- BILLRD.py
+++ BILLRD.py
@@ -1,30 +1,29 @@
 #Filename : BILLRD.py
 
-t = int(input())
-
+#Accept inputs
 for t in range(int(input())):
-    n ,k, x, y = map(input().split())
+    n ,k, x, y = map(int,input().split())
+    
+    #Check if ball is on 
+    #a coordinate that would
+    #pot the ball
+    
     if x == y:
         print(n,n)
+    
     else:
-        l = []
+        #List all 4 points that 
+        #the ball would collide
+        poc = []
+        
+        
         if x < y:
             poc = [[x+n-y, n],[n, n-y+x],[y-x, 0],[0, y-x]]
-            if x < y:
-                poc = []
-                pass
-            else:
-                poc = [[n, y+n-x], [y+n-x,n],[0, x-y], [x-y, 0]]
+        
+        else:
+            poc = [[n, y+n-x], [y+n-x,n],[0, x-y], [x-y, 0]]
+        
+        # k%4 = k since the ball rotates within these points
         fp = poc[(k-1)%4]
+        
         print(fp[0], fp[1])
-
-
-
-
-
-
-
-
-
-
-
