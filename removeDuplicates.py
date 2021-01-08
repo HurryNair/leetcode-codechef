e8c3e52f729c225ef7e88e70404bc8f4c669997b
Harishankar S <67646220+Hurry-Nair@users.noreply.github.com>
2021-01-08 08:37:00 +0530

    removeDuplicates

---
+++ removeDuplicates.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        else:
+            #First item will be unique
+            #Check if second item is unique
+            #In comparison to first
+            #If not replace with next unique item
+            #Repeat
+            i = 1 
+            for j in range(1, len(nums)):
+                if nums[j] != nums[j-1]:
+                    nums[i] = nums[j]
+                    i += 1
+            return i
