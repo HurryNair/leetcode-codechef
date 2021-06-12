7430919a18e7e790369fe80c3cba5acf1b353171
hari <hari.nair634@gmail.com>
2021-06-12 18:40:12 +0530

    Updated amazonprep

--- Amazon/highest_powerof2.py
+++ Amazon/highest_powerof2.py
@@ -21,5 +21,19 @@ print(highestpowerof2(500))
      
 print("#"*40)
 
+# Application problems
+
+"""
+Some people are standing in a queue. A selection process follows a rule where people standing on even positions are selected. 
+Of the selected people a queue is formed and again out of these only people on even position are selected. 
+This continues until we are left with one person. Find out the position of that person in the original queue. 
+Print the position(original queue) of that person who is left. 
+"""
+
+
+
+
+
+
 
 
