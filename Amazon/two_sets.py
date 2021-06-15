ff5f60585965604dee26b8ca32b48dff623b7b4d
hari <hari.nair634@gmail.com>
2021-06-15 22:16:40 +0530

    ama

---
+++ Amazon/two_sets.py
@@ -0,0 +1,11 @@
+"""
+Given a number n, verify if its possible to divide the numbers 1,2,3...n into two sets of equal sum
+Print "YES" or "NO"
+If "YES" print the number of elements as well as the elements in each set
+
+
+
+https://www.youtube.com/watch?v=dZ_6MS14Mg4
+https://www.youtube.com/watch?v=QGVCnjXmrNg
+
+"""
