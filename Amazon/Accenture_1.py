1f2825413ea173c307a4900bb4c859bc24e8e712
hari <hari.nair634@gmail.com>
2022-05-25 12:41:39 +0530

    acc_gcp

---
+++ Amazon/Accenture_1.py
@@ -0,0 +1,19 @@
+"""
+Given two integers m and n, return max number of sequences of integers that can be formed of length n,
+such that all values are lesser than or equal to value m.
+
+As an added condition, the consecutive numbers must be twice as much as its preceding number or greater
+
+Example:
+
+m = 10; n = 4
+
+1, 2, 4, 8
+1, 2, 4, 10
+1, 2, 5, 8
+1, 2, 5, 10
+1, 3, 5, 8
+1, 3, 5, 10 etc
+
+"""
+
---
+++ Amazon/Accenture_2.py
@@ -0,0 +1,5 @@
+"""
+FOreword: The most vague and poorly worded question I have ever seen
+
+Given 
+"""
