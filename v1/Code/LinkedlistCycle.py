c5ece5dd6e8d5401edeebb34c9b1742c3f6d0466
Harishankar S <harishankar.s@hpe.com>
2022-07-11 21:31:16 +0530

    LinkedListCycle

---
+++ v1/Code/LinkedlistCycle.py
@@ -0,0 +1,22 @@
+# 2 pointer approach with a fast and a slow pointer
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while(fast != None and fast.next != None):
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+        
+        return False
+            
+            
+        
